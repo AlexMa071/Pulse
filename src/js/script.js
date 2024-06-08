c2b1fad73bb06c1d3044f1652582f59567279b70
Alex <example@gmail.com>
2024-06-08 12:58:57 +0200

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -44,7 +44,8 @@ $(document).ready(function () {
     $("[data-modal=consultation]").on("click", function () {
         $(".overlay, #consultation").fadeIn();
     });
-    $(".modal__close").on("click", function () {
+    $(".modal__close").on("click", function (e) 
+    {
         $(".overlay, #consultation, #thanks, #order").fadeOut();
     });
 
@@ -53,5 +54,57 @@ $(document).ready(function () {
             $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
             $(".overlay, #order").fadeIn();
         });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!"),
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты",
+                },
+            },
+        });
+    }
+
+    validateForms("#consultation-form");
+    validateForms("#consultation form");
+    validateForms("#order form");
+
+    $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if(!$(this).valid()){
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
     });
 });
